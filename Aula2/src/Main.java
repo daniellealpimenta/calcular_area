937183ebe717323bb41d7b761f6bf92d6ffbbc32
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2024-09-26 11:14:53 -0300

    Add files via upload

---
+++ Aula2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Forma[] formas = new Forma[3];
+
+
+        formas[0] = new Circulo(5); 
+        formas[1] = new Retangulo(4, 6);
+        formas[2] = new Triangulo(3, 8);
+
+
+        for (Forma forma : formas) {
+            System.out.printf("Área: %.2f\n", forma.calcularArea());
+        }
+    }
+}
